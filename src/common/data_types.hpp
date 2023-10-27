33c8ee1ee2f33f765e2ad427984d7a34e20f318d
Federico Silvestri <federico.silvestri@vedrai.com>
2023-10-27 19:29:39 +0200

    Add alpha len

--- src/common/data_types.hpp
+++ src/common/data_types.hpp
@@ -4,14 +4,16 @@
 
 #ifndef SPM_PROJECT_DATA_TYPES_HPP
 #define SPM_PROJECT_DATA_TYPES_HPP
-
 #include "array"
 #include "string"
+
+// Defining the alphabet len, ASCII is 256
+#define ALPHABET_LEN 256
 
 /**
  * @brief Definition of Frequency Map as a static fixed size array.
  */
-struct FrequencyMap : public std::array<unsigned int, 256> {
+struct FrequencyMap : public std::array<unsigned int, ALPHABET_LEN> {
     /*
      * Overload the constructor to fill entire array to 0.
      */
@@ -23,7 +25,7 @@ struct FrequencyMap : public std::array<unsigned int, 256> {
 /**
  * @brief Here I define for simplicity a new type for HuffMap.
  */
-struct HuffMap : public std::array<std::string, 256> {};
+struct HuffMap : public std::array<std::string, ALPHABET_LEN> {};
 
 
 #endif //SPM_PROJECT_DATA_TYPES_HPP
